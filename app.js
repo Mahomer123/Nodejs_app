eea5b426175ec378df7689d1ea5ab916ffb6eff7
Mahomer123 <160807173+Mahomer123@users.noreply.github.com>
2025-04-08 13:20:13 -0400

    Update app.js
    Adding a title "Welcome to Ottawa's Visitor Center"

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Let's Go DevOps</title>
+      <title>Welcome to Ottawa's Visitor Center/title>
       <style>
         body {
           margin: 0;
@@ -42,7 +42,7 @@ app.get('/', (req, res) => {
       </style>
     </head>
     <body>
-      <h1>Let's Go DevOps</h1>
+      <h1>Let's Go Ottawa</h1>
       <canvas id="fireworksCanvas"></canvas>
 
       <script>
